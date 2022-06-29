1cd844cb7936363afcf8c983e943b130c8e4c887
Omodamola Oladejim <darmhoo@hotmail.com>
2022-06-29 16:50:24 +0000

    fixed socialite

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,8 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('social_id');
+            $table->string('social_type');
             $table->rememberToken();
             $table->timestamps();
         });
